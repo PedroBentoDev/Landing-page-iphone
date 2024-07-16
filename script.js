04341463a8bcf3bf84f365d07621ba5f10450021
PedroBentoDev <157599423+PedroBentoDev@users.noreply.github.com>
2024-07-16 12:55:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function imgSlider(e){
+    document.querySelector('#phone').src = e;
+}
+function circleChange(color){
+    const circle= documn.querySelector('.circle');
+    circle.style.background = color;
+}
